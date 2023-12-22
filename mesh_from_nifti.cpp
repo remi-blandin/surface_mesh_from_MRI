43ec8cdf704f07ad72436ce5c459e82c4031e9cb
remi-kn <remi.blandin@gmail.com>
2023-12-22 17:53:09 +0100

    Process automatically several files using a configuration file.

--- mesh_from_nifti.cpp
+++ mesh_from_nifti.cpp
@@ -59,162 +59,227 @@ namespace PMP = CGAL::Polygon_mesh_processing;
 
 int main(int argc, char *argv[]) {
 
+  QApplication app(argc, argv);
+
+
+
   //*********************************************
-  // Load Nifti file
+  // Load the mesh extraction parameter file
   //*********************************************
   
   // create the file dialog
-  QApplication app(argc, argv);
   QString fileName = QFileDialog::getOpenFileName(nullptr, "Open File", "./images", "All Files (*.*)");
-  std::cout << "Processing file:\n" << fileName.toStdString() << std::endl;
   
-  // load NifTi image with VTK
-  vtkNew<vtkNIFTIImageReader> reader;
-  reader->SetFileName(fileName.toStdString().c_str()); 
-//  reader->SetFileName("images/tense-a-envelop2.nii.gz"); // tense-a-envelop
-  reader->Update();
-  auto vtk_image = reader->GetOutput();
-  Gray_level_image image(CGAL::IO::read_vtk_image_data(vtk_image), 2.9f);
+  std::ifstream inputFile(fileName.toStdString());
+  std::string line, item;
+  char separator(';');
+  int cnt;
+  double coord[3];
+  std::vector<std::string> fileNameList;
+  std::vector<Point_3> sphereCenterList;
   
-  // print the dimensions of the image
-  std::cout << "xdim: " << image.xdim() 
-  << " ydim: " << image.ydim()
-  << " zdim: " << image.zdim() << std::endl;
+  if (!inputFile.is_open())
+  {
+    std::cout << "Cannot open " << fileName.toStdString() << endl;
+    return(false);
+  }
+  else if (inputFile.peek() != std::ifstream::traits_type::eof())
+  {
+    while (getline(inputFile, line))
+    {
+      std::stringstream coordLine(line);
+      cnt = 0;
+      for (int i(0); i < 3; i++){coord[i] = 0.;}
+      
+      while (getline(coordLine, item, separator) && (cnt < 4))
+      {
+        if (item.size() > 0)
+        {
+          if (cnt == 0)
+          {
+            fileNameList.push_back(item);
+            cnt ++;
+          }
+          else
+          {
+            try
+            {
+              coord[cnt-1] = std::stod(item);
+              cnt++;
+            }
+            catch (const std::invalid_argument& ia) {
+              std::cout << ia.what() << endl;
+            }
+            
+            sphereCenterList.push_back(Point_3(coord[0], coord[1], coord[2]));
+          }
+        }
+      }
+    }
+    for (auto fi : fileNameList)
+    {
+      std::cout << fi << endl;
+    }
+    for (auto pt : sphereCenterList)
+    {
+      std::cout << pt << endl;
+    }
+  }
   
-    // print the dimensions of the image
-  std::cout << "vx: " << image.vx() 
-  << " vy: " << image.vy()
-  << " vz: " << image.vz() << std::endl;
-  
-      // print the dimensions of the image
-  std::cout << "tx: " << image.tx() 
-  << " ty: " << image.ty()
-  << " tz: " << image.tz() << std::endl;
-  
-
   //*********************************************
   // Request meshing parameters
   //*********************************************
 
-    // Prompt the user for a number using QInputDialog and get the result
-    bool ok;
-    double max_dim = QInputDialog::getDouble(nullptr, "Maximal dimension", "Enter mesh maximal dimension (mm)", 5., 0., 100., 1, &ok);
+  // Prompt the user for a number using QInputDialog and get the result
+  bool ok;
+  double max_dim = QInputDialog::getDouble(nullptr, "Maximal dimension", "Enter mesh maximal dimension (mm)", 1., 0., 100., 1, &ok);
     
-    double center_x = QInputDialog::getDouble(nullptr, "Center X", "Enter x coordinate of the center (mm)", double(image.xdim()) / 2., -1000., 1000., 1, &ok);
+//      double center_x = QInputDialog::getDouble(nullptr, "Center X", "Enter x coordinate of the center (mm)", double(image.xdim()) / 2., -1000., 1000., 1, &ok);
+//      
+//      double center_y = QInputDialog::getDouble(nullptr, "Center Y", "Enter y coordinate of the center (mm)", double(image.ydim()) / 2., -1000., 1000., 1, &ok);
+//      
+//      double center_z = QInputDialog::getDouble(nullptr, "Center Z", "Enter z coordinate of the center (mm)", double(image.zdim()) / 2., -1000., 1000., 1, &ok);
     
-    double center_y = QInputDialog::getDouble(nullptr, "Center Y", "Enter y coordinate of the center (mm)", double(image.ydim()) / 2., -1000., 1000., 1, &ok);
-    
-    double center_z = QInputDialog::getDouble(nullptr, "Center Z", "Enter z coordinate of the center (mm)", double(image.zdim()) / 2., -1000., 1000., 1, &ok);
-    
-    double stop_ratio = QInputDialog::getDouble(nullptr, "Percentage simplification", "Enter the percentage of edges to remove", 10., 0., 100., 1, &ok);
-    stop_ratio /= 100.;
-
-
-  //*********************************************
-  // Create surface mesh
-  //*********************************************
+  double stop_ratio = QInputDialog::getDouble(nullptr, "Percentage simplification", "Enter the percentage of edges to remove", 10., 0., 100., 1, &ok);
+  stop_ratio /= 100.;
   
   if (ok){
   
-    std::cout << "Mesh will be generated with maximal dimension " << max_dim 
-      << " mm" << std::endl; 
-
-    Tr tr;            // 3D-Delaunay triangulation
-    C2t3 c2t3 (tr);   // 2D-complex in 3D-Delaunay triangulation
-
-    // Carefully chosen bounding sphere: the center must be inside the
-    // surface defined by 'image' and the radius must be high enough so that
-    // the sphere actually bounds the whole image.
-    GT::Point_3 bounding_sphere_center(
-      center_x,
-      center_y,
-      center_z);
-//    GT::Point_3 bounding_sphere_center(double(image.xdim()) / 2.,
-//      double(image.ydim()) / 2.,
-//      double(image.zdim()) / 2.);
-    double radius_mm = 260.;
-
-    GT::FT bounding_sphere_squared_radius = radius_mm * radius_mm;
-    GT::Sphere_3 bounding_sphere(bounding_sphere_center,
-                                     bounding_sphere_squared_radius);
-
-    // definition of the surface, with 10^-5 as relative precision
-    Surface_3 surface(image, bounding_sphere, 1e-5);
-
-    // defining meshing criteria
-    CGAL::Surface_mesh_default_criteria_3<Tr> criteria(30.,
-                                                       max_dim,
-                                                       max_dim);
-
-    // meshing surface, with the "manifold without boundary" algorithm
-    CGAL::make_surface_mesh(c2t3, surface, criteria, CGAL::Manifold_tag());
-    
-  //*********************************************
-  // Export generated mesh
-  //*********************************************
-  
-    std::ofstream out("out.off");
-    CGAL::output_surface_facets_to_off (out, c2t3);
-    std::cout << "Final number of points: " << tr.number_of_vertices() << "\n";
-    
-  //*********************************************
-  // Smooth mesh
-  //*********************************************
-  
-    const std::string filename_smooth = (argc > 1) ? argv[1] : CGAL::data_file_path("out.off");
-
-    Mesh mesh;
-    if(!PMP::IO::read_polygon_mesh(filename_smooth, mesh))
+    for (int img(0); img < fileNameList.size(); img ++)
     {
-      std::cerr << "Invalid input." << std::endl;
-      return 1;
-    }
-    
-    const unsigned int nb_iterations = (argc > 2) ? std::atoi(argv[2]) : 6;
-    const double time = (argc > 3) ? std::atof(argv[3]) : 0.05;
-  
-    std::set<Mesh::Vertex_index> constrained_vertices;
-    for(Mesh::Vertex_index v : vertices(mesh))
-    {
-      if(is_border(v, mesh))
-        constrained_vertices.insert(v);
-    }
-    
-    std::cout << "Constraining: " << constrained_vertices.size() << " border vertices" << std::endl;
-    CGAL::Boolean_property_map<std::set<Mesh::Vertex_index> > vcmap(constrained_vertices);
-    
-    std::cout << "Smoothing shape... (" << nb_iterations << " iterations)" << std::endl;
-    PMP::smooth_shape(mesh, time, PMP::parameters::number_of_iterations(nb_iterations)
-                                                  .vertex_is_constrained_map(vcmap));
-    CGAL::IO::write_polygon_mesh("out_smoothed.off", mesh, CGAL::parameters::stream_precision(17));
-    std::cout << "Done!" << std::endl;
-    
-  //*********************************************
-  // Simplify mesh
-  //*********************************************
-  
-    SMS::Count_ratio_stop_predicate<Surface_mesh> stop(stop_ratio);
-  
-    Surface_mesh surface_mesh;
-    const std::string filename = (argc > 1) ? argv[1] : CGAL::data_file_path("out_smoothed.off");
-    std::ifstream is(filename);
-    if(!is || !(is >> surface_mesh))
-    {
-      std::cerr << "Failed to read input mesh: " << filename << std::endl;
-      return EXIT_FAILURE;
-    }
-    if(!CGAL::is_triangle_mesh(surface_mesh))
-    {
-      std::cerr << "Input geometry is not triangulated." << std::endl;
-      return EXIT_FAILURE;
-    }
+      
+      //*********************************************
+      // Load Nifti file
+      //*********************************************
+      
+      std::cout << "Processing file:\n" << fileNameList[img] << std::endl;
+      
+      // load NifTi image with VTK
+      vtkNew<vtkNIFTIImageReader> reader;
+      reader->SetFileName(fileNameList[img].c_str()); 
+    //  reader->SetFileName("images/tense-a-envelop2.nii.gz"); // tense-a-envelop
+      reader->Update();
+      auto vtk_image = reader->GetOutput();
+      Gray_level_image image(CGAL::IO::read_vtk_image_data(vtk_image), 2.9f);
+      
+      // print the dimensions of the image
+      std::cout << "xdim: " << image.xdim() 
+      << " ydim: " << image.ydim()
+      << " zdim: " << image.zdim() << std::endl;
+      
+        // print the dimensions of the image
+      std::cout << "vx: " << image.vx() 
+      << " vy: " << image.vy()
+      << " vz: " << image.vz() << std::endl;
+      
+          // print the dimensions of the image
+      std::cout << "tx: " << image.tx() 
+      << " ty: " << image.ty()
+      << " tz: " << image.tz() << std::endl;
+      
 
+      //*********************************************
+      // Create surface mesh
+      //*********************************************
     
-    int r = SMS::edge_collapse(surface_mesh, stop);
-  
-    std::cout << "\nFinished!\n" << r << " edges removed.\n" << surface_mesh.number_of_edges() << " final edges.\n";
+      std::cout << "Mesh will be generated with maximal dimension " << max_dim 
+        << " mm" << std::endl; 
+
+      Tr tr;            // 3D-Delaunay triangulation
+      C2t3 c2t3 (tr);   // 2D-complex in 3D-Delaunay triangulation
+
+      // Carefully chosen bounding sphere: the center must be inside the
+      // surface defined by 'image' and the radius must be high enough so that
+      // the sphere actually bounds the whole image.
+      GT::Point_3 bounding_sphere_center(
+        sphereCenterList[img].x(),
+        sphereCenterList[img].y(),
+        sphereCenterList[img].z());
+
+      double radius_mm = 260.;
+
+      GT::FT bounding_sphere_squared_radius = radius_mm * radius_mm;
+      GT::Sphere_3 bounding_sphere(bounding_sphere_center,
+                                       bounding_sphere_squared_radius);
+
+      // definition of the surface, with 10^-5 as relative precision
+      Surface_3 surface(image, bounding_sphere, 1e-5);
+
+      // defining meshing criteria
+      CGAL::Surface_mesh_default_criteria_3<Tr> criteria(30.,
+                                                         max_dim,
+                                                         max_dim);
+
+      // meshing surface, with the "manifold without boundary" algorithm
+      CGAL::make_surface_mesh(c2t3, surface, criteria, CGAL::Manifold_tag());
+      
+    //*********************************************
+    // Export generated mesh
+    //*********************************************
     
-    CGAL::IO::write_polygon_mesh((argc > 3) ? argv[3] : "out_simp.off", surface_mesh, CGAL::parameters::stream_precision(17));
+      std::ofstream out("out.off");
+      CGAL::output_surface_facets_to_off (out, c2t3);
+      std::cout << "Final number of points: " << tr.number_of_vertices() << "\n";
+      
+    //*********************************************
+    // Smooth mesh
+    //*********************************************
+    
+      const std::string filename_smooth = (argc > 1) ? argv[1] : CGAL::data_file_path("out.off");
+
+      Mesh mesh;
+      if(!PMP::IO::read_polygon_mesh(filename_smooth, mesh))
+      {
+        std::cerr << "Invalid input." << std::endl;
+        return 1;
+      }
+      
+      const unsigned int nb_iterations = (argc > 2) ? std::atoi(argv[2]) : 6;
+      const double time = (argc > 3) ? std::atof(argv[3]) : 0.05;
+    
+      std::set<Mesh::Vertex_index> constrained_vertices;
+      for(Mesh::Vertex_index v : vertices(mesh))
+      {
+        if(is_border(v, mesh))
+          constrained_vertices.insert(v);
+      }
+      
+      std::cout << "Constraining: " << constrained_vertices.size() << " border vertices" << std::endl;
+      CGAL::Boolean_property_map<std::set<Mesh::Vertex_index> > vcmap(constrained_vertices);
+      
+      std::cout << "Smoothing shape... (" << nb_iterations << " iterations)" << std::endl;
+      PMP::smooth_shape(mesh, time, PMP::parameters::number_of_iterations(nb_iterations)
+                                                    .vertex_is_constrained_map(vcmap));
+      CGAL::IO::write_polygon_mesh("out_smoothed.off", mesh, CGAL::parameters::stream_precision(17));
+      std::cout << "Done!" << std::endl;
+      
+    //*********************************************
+    // Simplify mesh
+    //*********************************************
+    
+      SMS::Count_ratio_stop_predicate<Surface_mesh> stop(stop_ratio);
+    
+      Surface_mesh surface_mesh;
+      const std::string filename = (argc > 1) ? argv[1] : CGAL::data_file_path("out_smoothed.off");
+      std::ifstream is(filename);
+      if(!is || !(is >> surface_mesh))
+      {
+        std::cerr << "Failed to read input mesh: " << filename << std::endl;
+        return EXIT_FAILURE;
+      }
+      if(!CGAL::is_triangle_mesh(surface_mesh))
+      {
+        std::cerr << "Input geometry is not triangulated." << std::endl;
+        return EXIT_FAILURE;
+      }
+
+      
+      int r = SMS::edge_collapse(surface_mesh, stop);
+    
+      std::cout << "\nFinished!\n" << r << " edges removed.\n" << surface_mesh.number_of_edges() << " final edges.\n";
+      
+      CGAL::IO::write_polygon_mesh((argc > 3) ? argv[3] : "out_simp.off", surface_mesh, CGAL::parameters::stream_precision(17));
+    }
   
   }
   else
